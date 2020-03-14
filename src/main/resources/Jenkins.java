32fa5136b53193c9ca29433e95a33fdc031cb012
kzhic <kzhic2013@gmail.com>
2020-03-14 15:34:59 +0800

    test modified

--- src/main/resources/Jenkins.java
+++ src/main/resources/Jenkins.java
@@ -4,9 +4,10 @@ public class Jenkins {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello we are here");
+		System.out.println("测试一下修改");
 	}
 	
 	public static void run(int i){
-	
+
 	}
 }
